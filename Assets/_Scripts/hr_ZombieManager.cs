70ae7a13b6a5a976bb6fac892b02e70cb7417641
VFermat <vitor.eller30@gmail.com>
2021-05-27 16:05:50 -0300

    Add random scared shouts to player on zombie encounter

--- Assets/_Scripts/hr_ZombieManager.cs
+++ Assets/_Scripts/hr_ZombieManager.cs
@@ -12,6 +12,7 @@ public class hr_ZombieManager : MonoBehaviour
     private GameObject player;
     private NavMeshAgent agent;
     private Renderer renderer;
+    
 
     /// <summary>
     /// Start is called on the frame when a script is enabled just before
@@ -55,5 +56,6 @@ public class hr_ZombieManager : MonoBehaviour
     public void OnAware()
     {
         isAware = true;
+        FindObjectOfType<AudioManager>().PlayScaredShout();
     }
 }
